3a9374a95b7cd026ecb575d859bd3ce7ec9495f2
Sri-Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2023-11-23 23:06:27 +0530

    Add files via upload

---
+++ 14.sizes_datatypes.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    printf("%d\n%d\n%d\n%d",sizeof(int),sizeof(float),sizeof(char),sizeof(double));
+    return 0;
+}
+
