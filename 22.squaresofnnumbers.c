58d2561a799fd4b1a8885820d506cad68b87d021
Sri-Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2024-05-17 17:04:30 +0530

    Add files via upload

---
+++ 22.squaresofnnumbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n,i;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        printf("%d\n",i*i);
+    }
+}
+
