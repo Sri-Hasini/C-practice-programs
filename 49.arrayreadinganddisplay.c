02369a60372819a462c458135df3dbd8b6088890
Sri-Hasini <112327981+Sri-Hasini@users.noreply.github.com>
2024-05-24 22:54:42 +0530

    Add files via upload

---
+++ 49.arrayreadinganddisplay.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    int n,i;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<n;i++){
+        printf("%d\n",a[i]);
+    }
+}
+
